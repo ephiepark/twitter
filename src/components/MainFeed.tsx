3b9e4507df9a30abee139c3f33e82a900d98a51c
Ephraim Park <ephiepark@gmail.com>
2022-10-04 23:23:07 -0700

    Add TweetInputField

--- src/components/MainFeed.tsx
+++ src/components/MainFeed.tsx
@@ -1,7 +1,32 @@
 import { WithFirebaseApiProps, withFirebaseApi } from "../Firebase";
+import { Box, Stack, TextField, Button } from "@mui/material";
+import { useState } from "react";
+
+const TweetInputField = () => {
+  const [tweetContent, setTweetContent] = useState<string>('');
+
+  const onSubmit = () => {
+    setTweetContent("");
+  };
+
+  return (
+    <Box sx={{ margin: "auto" }}>
+      <Stack direction="row" spacing={2} justifyContent="center">
+        <TextField
+          id="tweet-input"
+          label="Tweet"
+          variant="outlined"
+          onChange={(e) => setTweetContent(e.target.value)} value={tweetContent}
+        />
+        <Button variant="outlined" onClick={onSubmit}>Submit</Button>
+      </Stack>
+    </Box>
+  );
+};
 
 const MainFeedBase = (props: WithFirebaseApiProps) => {
   return (<>
+    <TweetInputField />
   </>);
 }
 
