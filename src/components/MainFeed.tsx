b9678a03a4d618bb6c6835ad72774b909f150ef9
Ephraim Park <ephiepark@gmail.com>
2022-10-04 23:17:30 -0700

    Add MainFeed

---
+++ src/components/MainFeed.tsx
@@ -0,0 +1,8 @@
+import { WithFirebaseApiProps, withFirebaseApi } from "../Firebase";
+
+const MainFeedBase = (props: WithFirebaseApiProps) => {
+  return (<>
+  </>);
+}
+
+export default withFirebaseApi(MainFeedBase);
