b42262b5427bb4d5d0bae7ac082a032aa3f8213a
Ephraim Park <ephiepark@gmail.com>
2022-10-05 23:04:53 -0700

    Add asyncGetMainFeed to FirebaseApi

--- src/types.ts
+++ src/types.ts
@@ -8,8 +8,14 @@ export interface WithLoadingState {
   loadState: 'idle' | 'loading' | 'failed';
 };
 
+export interface WithId {
+  id: string;
+};
+
 export interface Tweet {
   userId: string;
   tweetContent: string;
   createdTime: number;
 };
+
+export type TweetWithId = Tweet & WithId;
